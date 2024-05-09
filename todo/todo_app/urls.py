7afb060e5370bc8e6c4b93bb65ebeddac9c9a198
Pola Qulta <poequlta199@gmail.com>
2024-05-09 18:54:43 +0300

    Added auth tests

--- todo/todo_app/urls.py
+++ todo/todo_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage, TaskReorder
 from django.contrib.auth.views import LogoutView
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
-
-    path('', TaskList.as_view(), name='tasks'),
+    #path('', TaskList.as_view(), name='tasks'),
+    path('',TemplateView.as_view(template_name="todo_app/main.html"),name="tasks"),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
