022b5162d2129f1b2a81260b6c042cffb3617404
Pola Qulta <poequlta199@gmail.com>
2024-05-09 19:26:30 +0300

    Added Auth views

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -19,15 +19,34 @@ from .forms import PositionForm
 
 
 class CustomLoginView(LoginView):
-    pass
+    template_name = 'todo_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 
 class RegisterPage(FormView):
-    pass
+    template_name = 'todo_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
-    pass
+    template_name = 'todo_app/main.html'
 
 
 class TaskDetail(LoginRequiredMixin, DetailView):
