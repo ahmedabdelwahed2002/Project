8875b9a5de884349c8bb6bcebfa9e00499a41e8d
Ahmed Abdelwahed <88972064+ahmedabdelwahed2002@users.noreply.github.com>
2024-05-15 00:17:07 +0300

    handeployyyy !!

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t4wtt0k5&x19l2gsaeazb*24x^1tlcw=_&lmqa*9br790qndz5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
