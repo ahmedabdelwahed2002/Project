05b49b5b0928eef43b360b44df62124e1aa271bc
Pola Qulta <poequlta199@gmail.com>
2024-05-14 03:26:09 +0300

    Added more views

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -46,22 +46,62 @@ class RegisterPage(FormView):
 
 
 class TaskList(LoginRequiredMixin, ListView):
-    template_name = 'todo_app/main.html'
+    model = Task
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__contains=search_input)
+
+        context['search_input'] = search_input
+
+        return context
 
 
 class TaskDetail(LoginRequiredMixin, DetailView):
-    pass
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todo_app/task.html'
 
 
 class TaskCreate(LoginRequiredMixin, CreateView):
-    pass
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
-    pass
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
 
 
 class DeleteView(LoginRequiredMixin, DeleteView):
-    pass
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
 
 class TaskReorder(View):
-    pass
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
+
+        return redirect(reverse_lazy('tasks'))
